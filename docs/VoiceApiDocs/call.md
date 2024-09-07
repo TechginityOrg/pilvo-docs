074aac99a1a0857ce5cd5eaf259ffc659c3f6040
Shivam Maheshwari <fadehack@Shivams-MacBook-Pro.local>
2024-09-08 00:34:30 +0530

    changes

--- docs/VoiceApiDocs/call.md
+++ docs/VoiceApiDocs/call.md
@@ -1,6 +1,7 @@
 ---
 hide_title: true
 hide_table_of_contents: true
+slug: /voice/api/call/
 ---
 
 import CustomLayout from '@site/src/components/CustomLayout';
--- sidebars.js
+++ sidebars.js
@@ -2,71 +2,56 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  apiSidebar: {
-    'Getting Started': [
-      {
-        type: 'category',
-        label: 'API Docs',
-        items: [
-          'apiDocs/list-all-messages.api',
-          'apiDocs/list-mms-media',
-          'apiDocs/plivo-message-api',
-          'apiDocs/retrieve-message',
-          'apiDocs/send-message',
-        ],
-        customProps: {
-          className: 'leftSidebar',
-        },
-      },
-      {
-        type: 'category',
-        label: 'Quick Start',
-        items: [
-          'quickStart/GoQuickstart',
-          'quickStart/JavaQuickstart',
-          'quickStart/node-expressjs',
-          'quickStart/php',
-          'quickStart/RubyQuickstart',
-        ],
-        customProps: {
-          className: 'leftSidebar',
-        },
-      },
-    ],
-  },
-  voiceApiSidebar: {
-    'Voice API Docs': [
-      {
-        type: 'category',
-        label: 'Voice API Docs',
-        items: [
-          'VoiceApiDocs/call',
-          'VoiceApiDocs/conference',
-          'VoiceApiDocs/plivo-message-api.info',
-          'VoiceApiDocs/request',
-          'VoiceApiDocs/response',
-        ],
-        customProps: {
-          className: 'leftSidebar',
-        },
-      },
-      {
-        type: 'category',
-        label: 'Voice API Quick Start',
-        items: [
-          'voiceApiQuickstart/DotNETQuickstart',
-          'voiceApiQuickstart/GoQuickstart',
-          'voiceApiQuickstart/JavaQuickstart',
-          'voiceApiQuickstart/NodejsQuickstart',
-          'voiceApiQuickstart/PHPQuickstart',
-          'voiceApiQuickstart/RubyQuickstart',
-        ],
-        customProps: {
-          className: 'leftSidebar',
-        },
-      },
-    ],
-  },
+  apiSidebar: [
+    {
+      type: 'category',
+      label: 'Quick Start',
+      items: [
+        'quickStart/GoQuickstart',
+        'quickStart/javaQuickstart',
+        'quickStart/node-expressjs',
+        'quickStart/php',
+        'quickStart/rubyquickstart',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'API Docs',
+      items: [
+        'apiDocs/list-all-messages.api',
+        'apiDocs/list-mms-media',
+        'apiDocs/plivo-message-api',
+        'apiDocs/retrieve-message',
+        'apiDocs/send-message',
+      ],
+    },
+
+  ],
+  voiceApiSidebar: [
+    {
+      type: 'category',
+      label: 'Quick Start',
+      items: [
+        'voiceApiQuickstart/DotNETQuickstart',
+        'voiceApiQuickstart/GoQuickstart',
+        'voiceApiQuickstart/JavaQuickstart',
+        'voiceApiQuickstart/NodejsQuickstart',
+        'voiceApiQuickstart/PHPQuickstart',
+        'voiceApiQuickstart/RubyQuickstart',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'API Docs',
+      items: [
+        'VoiceApiDocs/call',
+        'VoiceApiDocs/conference',
+        'VoiceApiDocs/plivo-message-api.info',
+        'VoiceApiDocs/request',
+        'VoiceApiDocs/response',
+      ],
+    },
+  ],
 };
 
 export default sidebars;
